6bdd538788cb70c232d9335c2591ef099361f4c1
mailmevijayakarthik <mailmevijayakarthik@gmail.com>
2019-10-05 14:57:39 -0500

    Commiting my python

---
+++ com/pythonscripts/maxsquarepossible.py
@@ -0,0 +1,10 @@
+
+def maxsquarespossible(x,y):
+    squares=(x-1)*(y-1)
+    print(squares)
+
+while True:
+    x=int(input("Enter the number of horizontal lines"))
+    y=int(input("Enter the number of vertical lines"))
+    maxsquarespossible(x,y)
+    
